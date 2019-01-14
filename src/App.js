fa86e4a1350b94d81635e1bb10810729451ad9a8
wanku-x <wanku@bk.ru>
2019-01-15 00:10:17 +0300

    Добавлены: роутер и фейковая проверка аутенцификации

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React, { Component, Fragment } from 'react';
-import { Header, Main, Footer } from './layouts';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MainLayout } from './layouts';
+import { Loader } from './components';
+
+const Home = (props) => (
+  <h1>Route /</h1>
+)
+
+const Settings = (props) => (
+  <h1>Route /settings</h1>
+)
+
+const Timetable = (props) => (
+  <h1>Route /timetable</h1>
+)
+
+const NotFound = (props) => (
+  <h1>NotFound</h1>
+)
 
 class App extends Component {
+  state = {
+    checkAuth: false,
+    isLoggedIn: false,
+  };
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({ checkAuth: true });
+    }, 5000);
+  }
+
+  setLoggedIn = (isLoggedIn) => {
+    this.setState({ isLoggedIn });
+  }
+
   render() {
-    return (
-      <Fragment>
-        <Header />
-        <Main />
-        <Footer />
-      </Fragment>
+    return this.state.checkAuth ? (
+      <Router>
+        <MainLayout>
+          <Switch>
+            <Route exact path="/" render={() => <Home />} />
+            <Route exact path="/settings" render={() => <Settings />} />
+            <Route exact path="/timetable" render={() => <Timetable />}/>
+            <Route component={NotFound} />
+          </Switch>
+        </MainLayout>
+      </Router>
+    ):(
+      <Loader isOpen={true} />
     );
   }
 }
