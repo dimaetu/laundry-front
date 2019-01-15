4dd2b5f015df293e22014eac781a850712a8375a
wanku-x <wanku@bk.ru>
2019-01-15 22:51:02 +0300

    Вынес в AuthProvider весь функционал проверки аутентификации пользователя

--- src/components/AuthProvider.js
+++ src/components/AuthProvider.js
@@ -1,34 +1,159 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+import Loader from './Loader';
  
 const { Provider, Consumer } = React.createContext({
   auth: false,
   dorm: null,
   floor: null,
   room: '',
+  setLoggedIn: () => (null),
+  logout: () => (null),
 });
  
 export class AuthProvider extends Component {
+  state = {
+    checkAuth: false,
+    auth: true,
+    dorm: 12,
+    floor: 23,
+    room: '1232',
+    setLoggedIn: this.setLoggedIn,
+    logout: this.logout,
+  };
+
+  setLoggedIn = (value) => {
+    this.setState({
+      auth: value.auth,
+      dorm: value.dorm,
+      floor: value.floor,
+      room: value.room,
+    });
+  }
+
+  logout = () => {
+    this.setState({
+      auth: false,
+      dorm: null,
+      floor: null,
+      room: '',
+    });
+  }
+
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({ checkAuth: true });
+    }, 1000);
+  }
+
   render() {
-    return <Provider value={this.props.value}>{this.props.children}</Provider>
+    const { checkAuth } = this.state;
+
+    return (
+      <Provider value={this.state}>
+        {checkAuth ? (
+          this.props.children
+        ):(
+          <Loader isOpen={true} />
+        )}
+      </Provider>
+    )
   }
 }
- 
-export const withAuth = Component => (props) => (
+
+export const upGuest = Component => (props) => (
   <Consumer>
     {
-      ({auth}) => (
-        auth ? <Component {...props} /> : <Redirect to="/" />
+      ({auth, dorm, floor, room, setLoggedIn, logout}) => (
+        <Component
+          auth={auth}
+          dorm={dorm}
+          floor={floor}
+          room={room}
+          setLoggedIn={setLoggedIn}
+          logout={logout}
+          {...props} 
+        />
+      )
+    }
+  </Consumer>
+)
+ 
+export const upAuth = Component => (props) => (
+  <Consumer>
+    {
+      ({auth, dorm, floor, room, setLoggedIn, logout}) => (
+        auth ?
+          <Component
+            auth={auth}
+            dorm={dorm}
+            floor={floor}
+            room={room}
+            setLoggedIn={setLoggedIn}
+            logout={logout}
+            {...props} 
+          />
+        :
+          <Redirect to="/" />
       )
     }
   </Consumer>
 )
 
-export const withAuthSettings = Component => props => (
+export const onlyGuest = Component => (props) => (
   <Consumer>
     {
-      ({auth, dorm, floor, room}) => (
-        auth && dorm && floor && room ? <Component {...props} /> : <Redirect to="/" />
+      ({auth, dorm, floor, room, setLoggedIn, logout}) => (
+        !auth &&
+        <Component
+          auth={auth}
+          dorm={dorm}
+          floor={floor}
+          room={room}
+          setLoggedIn={setLoggedIn}
+          logout={logout}
+          {...props} 
+        />
+      )
+    }
+  </Consumer>
+)
+ 
+export const onlyAuth = Component => (props) => (
+  <Consumer>
+    {
+      ({auth, dorm, floor, room, setLoggedIn, logout}) => (
+        auth && (!dorm || !floor || !room) &&
+        <Component
+          auth={auth}
+          dorm={dorm}
+          floor={floor}
+          room={room}
+          setLoggedIn={setLoggedIn}
+          logout={logout}
+          {...props} 
+        />
+      )
+    }
+  </Consumer>
+)
+
+export const onlyFullAuth = Component => (props) => (
+  <Consumer>
+    {
+      ({auth, dorm, floor, room, setLoggedIn, logout}) => (
+        auth && dorm && floor && room ? 
+          <Component
+            auth={auth}
+            dorm={dorm}
+            floor={floor}
+            room={room}
+            setLoggedIn={setLoggedIn}
+            logout={logout}
+            {...props}
+          />
+        :
+          <Redirect to="/" />
       )
     }
   </Consumer>
