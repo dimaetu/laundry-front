f858e43f0550952683968be6175ad6d773d0ae98
wanku-x <wanku@bk.ru>
2019-01-16 21:24:20 +0300

    Добавлено: Title

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import { indigo } from '@material-ui/core/colors'
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  font-family: 'GoshaSans-Bold';
+  font-size: 2rem;
+  color: ${ indigo[500] };
+
+  @media (min-width: 600px) {
+    font-size: 3rem;
+  }
+`;
+
+export default Title;
