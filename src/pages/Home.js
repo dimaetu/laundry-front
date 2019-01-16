9822d0d32a3e55e5a10c3fd5cab95e86aeac2d56
wanku-x <wanku@bk.ru>
2019-01-16 21:29:03 +0300

    Добавлено: блоки Reasons и HowTo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
-import { Jumbotron, AboutSite } from '../components';
+import { Jumbotron, AboutSite, Reasons, HowTo } from '../components';
 
 const Home = () => (
   <Fragment>
     <Jumbotron />
     <AboutSite />
+    <Reasons />
+    <HowTo />
   </Fragment>
 )
 
