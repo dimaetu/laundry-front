fb16e6e55de66879dcf368d08a3ec612351136a9
wanku-x <wanku@bk.ru>
2019-01-16 03:13:46 +0300

    Растянул и добавил отступы Main

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,12 +1,24 @@
 import React, { Component, Fragment } from 'react';
 import { Header, Footer } from './';
+import styled from 'styled-components';
+
+const Main = styled.main`
+  padding: 32px 16px;
+  flex-grow: 1;
+
+  @media (min-width: 600px) {
+    padding: 32px 24px;
+  }
+`;
 
 class MainLayout extends Component {
   render() {
     return (
       <Fragment>
         <Header />
-        {this.props.children}
+        <Main>
+          {this.props.children}
+        </Main>
         <Footer />
       </Fragment>
     );
