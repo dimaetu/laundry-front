97f81241867363c990962b38ff051bd7daa5971c
wanku-x <wanku@bk.ru>
2019-01-16 16:04:29 +0300

    Изменил базовый шрифт Material UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { MainLayout } from './layouts';
 import { Home, EndRegistration, Settings, Timetable, NotFound } from './pages';
 import { upAuth, onlyGuest, onlyAuth, onlyFullAuth } from './components';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      'sans-serif',
+    ].join(','),
+  },
+});
 
 class App extends Component {
   render() {
@@ -12,20 +22,22 @@ class App extends Component {
 
     return (
       <Router>
-        <MainLayout>
-          <Switch>
-            <Route exact path="/" render={() => (
-              <Fragment>
-                <AccessHome />
-                <AccessEndRegistration />
-                <AccessTimetable />
-              </Fragment>
-            )}/>
-            <Route exact path="/settings" component={upAuth(Settings)}/>
-            <Route exact path="/timetable" component={onlyFullAuth(Timetable)}/>
-            <Route component={NotFound} />
-          </Switch>
-        </MainLayout>
+        <MuiThemeProvider theme={theme}>
+          <MainLayout>
+            <Switch>
+              <Route exact path="/" render={() => (
+                <Fragment>
+                  <AccessHome />
+                  <AccessEndRegistration />
+                  <AccessTimetable />
+                </Fragment>
+              )}/>
+              <Route exact path="/settings" component={upAuth(Settings)}/>
+              <Route exact path="/timetable" component={onlyFullAuth(Timetable)}/>
+              <Route component={NotFound} />
+            </Switch>
+          </MainLayout>
+        </MuiThemeProvider>
       </Router>
     );
   }
