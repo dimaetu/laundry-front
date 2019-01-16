1825bd822a70d8172a08917bd091e3dd63f2aa93
wanku-x <wanku@bk.ru>
2019-01-16 16:05:03 +0300

    Создал базовый стиль для контейнера

---
+++ src/components/containerStyle.js
@@ -0,0 +1,19 @@
+export const containerStyle = `
+  margin: 0 auto;
+  padding-left: 16px;
+  padding-right: 16px;
+  box-sizing: border-box;
+  width: 100%;
+
+  @media (min-width: 600px) {
+    width: 560px;
+  }
+
+  @media (min-width: 960px) {
+    width: 920px;
+  }
+
+  @media (min-width: 1280px) {
+    width: 1240px;
+  }
+`;
