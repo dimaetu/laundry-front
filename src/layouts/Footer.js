f2069999d560c37b11ae50a00e9bd7c1cb6a3e2c
wanku-x <wanku@bk.ru>
2019-01-16 06:17:14 +0300

    Добавил контейнер

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -4,12 +4,29 @@ import { indigo } from '@material-ui/core/colors';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
-  padding: 32px 16px;
+  padding: 32px 0;
   min-height: 100px;
   background-color: #eee;
 
   @media (min-width: 600px) {
     padding: 32px 24px;
+  }
+`;
+
+const FooterContainer = styled.div`
+  margin: 0 auto;
+  padding: 0 16px;
+
+  @media (min-width: 600px) {
+    width: 560px;
+  }
+
+  @media (min-width: 960px) {
+    width: 920px;
+  }
+
+  @media (min-width: 1280px) {
+    width: 1220px;
   }
 `;
 
@@ -29,12 +46,14 @@ const ITCLink = styled.a`
 
 const Footer = () => (
   <StyledFooter>
-    <Typography variant="h6" component="p" gutterBottom>
-      Сделано <Link href="https://vk.com/wanku">Иваном</Link> и <Link href="https://vk.com/dimaetu">Дмитрием</Link><br />
-    </Typography>
-    <Typography variant="subtitle2" component="p" gutterBottom>
-      из команды <ITCLink href="https://vk.com/itc.digital"> ITC </ITCLink>
-    </Typography>
+    <FooterContainer>
+      <Typography variant="h6" component="p" gutterBottom>
+        Сделано <Link href="https://vk.com/wanku">Иваном</Link> и <Link href="https://vk.com/dimaetu">Дмитрием</Link><br />
+      </Typography>
+      <Typography variant="subtitle2" component="p" gutterBottom>
+        из команды <ITCLink href="https://vk.com/itc.digital"> ITC </ITCLink>
+      </Typography>
+    </FooterContainer>
   </StyledFooter>
 )
 export default Footer;
