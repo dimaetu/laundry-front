322a8765c20f994165c251b6d9495a4085bdf212
wanku-x <wanku@bk.ru>
2019-01-16 03:12:49 +0300

    Наведена красота в footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,6 +1,40 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import { indigo } from '@material-ui/core/colors';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  padding: 32px 16px;
+  min-height: 100px;
+  background-color: #eee;
+
+  @media (min-width: 600px) {
+    padding: 32px 24px;
+  }
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: ${ indigo[500] };
+`;
+
+const ITCLink = styled.a`
+  padding: 0 4px;
+  text-decoration: none;
+  background-color: ${ indigo[500] };
+  color: #fff;
+  font-size: 1.2em;
+  vertical-align: middle;
+`;
 
 const Footer = () => (
-    <h1>Damn! Footer!</h1>
+  <StyledFooter>
+    <Typography variant="h6" component="p" gutterBottom>
+      Сделано <Link href="https://vk.com/wanku">Иваном</Link> и <Link href="https://vk.com/dimaetu">Дмитрием</Link><br />
+    </Typography>
+    <Typography variant="subtitle2" component="p" gutterBottom>
+      из команды <ITCLink href="https://vk.com/itc.digital"> ITC </ITCLink>
+    </Typography>
+  </StyledFooter>
 )
 export default Footer;
