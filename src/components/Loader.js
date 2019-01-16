741bfbdb26b5c74bc9539af6e2b91afcb5a47cdd
wanku-x <wanku@bk.ru>
2019-01-16 16:07:00 +0300

    Пофиксил расположение лоадера position: fixed

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -6,7 +6,12 @@ import { Portal } from 'react-portal';
 import styled from 'styled-components'
 
 const StyledGrid = styled(Grid)`
-  height: 100vh;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
   background-color: rgba(0, 0, 0, 0.5);
   color: #fff;
 `
