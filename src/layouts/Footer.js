81c5d19316ea7a693faed9c7a2d3263a249dcac8
wanku-x <wanku@bk.ru>
2019-01-16 16:08:05 +0300

    Изменил контейнер в футере

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,32 +2,16 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import { indigo } from '@material-ui/core/colors';
 import styled from 'styled-components';
+import { containerStyle } from '../components';
 
 const StyledFooter = styled.footer`
   padding: 32px 0;
   min-height: 100px;
   background-color: #eee;
-
-  @media (min-width: 600px) {
-    padding: 32px 24px;
-  }
 `;
 
 const FooterContainer = styled.div`
-  margin: 0 auto;
-  padding: 0 16px;
-
-  @media (min-width: 600px) {
-    width: 560px;
-  }
-
-  @media (min-width: 960px) {
-    width: 920px;
-  }
-
-  @media (min-width: 1280px) {
-    width: 1220px;
-  }
+  ${containerStyle}
 `;
 
 const Link = styled.a`
