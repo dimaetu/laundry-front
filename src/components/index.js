0af47a85fda2885c6b0485b192cc3b548c837018
wanku-x <wanku@bk.ru>
2019-01-16 16:07:23 +0300

    Обновил импорты компонентов

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Loader from './Loader';
 import { AuthProvider, upGuest, upAuth, onlyGuest, onlyAuth, onlyFullAuth } from './AuthProvider';
+import Jumbotron from './Jumbotron';
+import { containerStyle } from './containerStyle';
+import AboutSite from './AboutWash';
 
 export {
   Loader,
   AuthProvider, upGuest, upAuth, onlyGuest, onlyAuth, onlyFullAuth,
+  Jumbotron,
+  containerStyle,
+  AboutSite,
 }
