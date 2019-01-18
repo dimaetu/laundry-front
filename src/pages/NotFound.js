9c70f54bd16eb57d98335ea1f05c7f0ca3622504
dimaetu <etu.s.u.p.r.a@gmail.com>
2019-01-18 22:06:51 +0300

    Стилизована страница NotFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+import { containerStyle, Title } from '../components';
 
-const NotFound = (props) => (
-  <h1>NotFound</h1>
+const Container = styled.section`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  ${containerStyle}
+`;
+
+const NotFound = () => (
+  <Container>
+    <Title>404</Title>
+    <Typography variant="h5" component="p" gutterBottom>
+      Страница не найдена :(
+    </Typography>
+  </Container>
 )
 
 export default NotFound;
