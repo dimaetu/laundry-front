9ab89cccabe253483705a2f3551eebff83c555f6
Iv4n84110 <vanka3087@gmail.com>
2019-01-23 00:31:05 +0300

    Отцентрированы селекты

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FilledInput, InputLabel, MenuItem, FormControl, Select, Fab  } from '@material-ui/core';
 import styled from 'styled-components';
 import { containerStyle, Title } from '../components';
+import Grid from '@material-ui/core/Grid';
 
 const Container = styled.section`
   padding-top: 32px;
@@ -49,57 +50,61 @@ class Settings extends Component {
   render() {
     return (
       <Container>
-        <Title>Настройки</Title>
-        <StyledControl variant="filled" style={{ marginBottom: 16 }}>
-          <InputLabel htmlFor="filled-dorm-simple">Общежитие</InputLabel>
-          <Select
-            value={this.state.dorm}
-            onChange={this.handleChange}
-            input={<FilledInput name="dorm" id="filled-dorm-simple" />}
-          >
-            <MenuItem value="">
-              <em>Не выбрано</em>
-            </MenuItem>
-            {this.state.dorms.map((dorm)=>(
-              <MenuItem value={dorm}>{dorm}</MenuItem>
-            ))}
-          </Select>  
-        </StyledControl>
-        <StyledControl variant="filled" style={{ marginBottom: 16 }}>
-          <InputLabel htmlFor="filled-floor-simple">Этаж</InputLabel>
-          <Select disabled={!this.state.dorm}
-            value={this.state.floor}
-            onChange={this.handleChange}
-            input={<FilledInput name="floor" id="filled-floor-simple" />}
-          >
-            <MenuItem value="">
-              <em>Не выбрано</em>
-            </MenuItem>
-            {this.state.floors.map((floor)=>(
-              <MenuItem value={floor}>{floor}</MenuItem>
-            ))}
-          </Select>
-        </StyledControl>
-        <StyledControl variant="filled" style={{ marginBottom: 16 }}>
-          <InputLabel htmlFor="filled-room-simple">Комната</InputLabel>
-          <Select disabled={!this.state.dorm || !this.state.floor}
-            value={this.state.room}
-            onChange={this.handleChange}
-            input={<FilledInput name="room" id="filled-room-simple" />}
-          >
-            <MenuItem value="">
-              <em>Не выбрано</em>
-            </MenuItem>
-            {this.state.rooms.map((room)=>(
-              <MenuItem value={room}>{room}</MenuItem>
-            ))}
-          </Select>
-        </StyledControl>  
-        <FabWrapper>
-          <Fab variant="extended" color="primary" disabled={!this.state.dorm || !this.state.floor || !this.state.room} component={Link} to="/">
-            Сохранить
-          </Fab>
-        </FabWrapper>
+        <Grid container justify="center">
+          <Grid item xs={12} md={6}>
+            <Title>Настройки</Title>
+            <StyledControl variant="filled" style={{ marginBottom: 16 }}>
+              <InputLabel htmlFor="filled-dorm-simple">Общежитие</InputLabel>
+              <Select
+                value={this.state.dorm}
+                onChange={this.handleChange}
+                input={<FilledInput name="dorm" id="filled-dorm-simple" />}
+              >
+                <MenuItem value="">
+                  <em>Не выбрано</em>
+                </MenuItem>
+                {this.state.dorms.map((dorm)=>(
+                  <MenuItem value={dorm}>{dorm}</MenuItem>
+                ))}
+              </Select>  
+            </StyledControl>
+            <StyledControl variant="filled" style={{ marginBottom: 16 }}>
+              <InputLabel htmlFor="filled-floor-simple">Этаж</InputLabel>
+              <Select disabled={!this.state.dorm}
+                value={this.state.floor}
+                onChange={this.handleChange}
+                input={<FilledInput name="floor" id="filled-floor-simple" />}
+              >
+                <MenuItem value="">
+                  <em>Не выбрано</em>
+                </MenuItem>
+                {this.state.floors.map((floor)=>(
+                  <MenuItem value={floor}>{floor}</MenuItem>
+                ))}
+              </Select>
+            </StyledControl>
+            <StyledControl variant="filled" style={{ marginBottom: 16 }}>
+              <InputLabel htmlFor="filled-room-simple">Комната</InputLabel>
+              <Select disabled={!this.state.dorm || !this.state.floor}
+                value={this.state.room}
+                onChange={this.handleChange}
+                input={<FilledInput name="room" id="filled-room-simple" />}
+              >
+                <MenuItem value="">
+                  <em>Не выбрано</em>
+                </MenuItem>
+                {this.state.rooms.map((room)=>(
+                  <MenuItem value={room}>{room}</MenuItem>
+                ))}
+              </Select>
+            </StyledControl>  
+            <FabWrapper>
+              <Fab variant="extended" color="primary" disabled={!this.state.dorm || !this.state.floor || !this.state.room} component={Link} to="/">
+                Сохранить
+              </Fab>
+            </FabWrapper>
+          </Grid>
+        </Grid>
       </Container>
     )
   }
