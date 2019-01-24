574ef40e65d7e33c08c193bd9643a281d9a0223f
Iv4n84110 <vanka3087@gmail.com>
2019-01-24 22:41:43 +0300

    Добавил кнопки вперед, назад в таблицу с расписанием

--- src/pages/Timetable.js
+++ src/pages/Timetable.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import styled from 'styled-components';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
 
-const Timetable = (props) => (
-  <h1>Timetable</h1>
-)
+
+
+
+class Timetable extends Component {
+  state = {
+    loaded: false,
+    dormId: null,
+    floorId: null,
+    roomId: null,
+    dorms: [],
+    floors: [],
+    rooms: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <Button 
+        variant="contained" 
+        href="#contained-buttons"
+        component={Link}
+        to="/"
+        > 
+            <ChevronLeft/>
+      </Button>
+      <Button 
+      variant="contained" 
+      href="#contained-buttons"
+      component={Link}
+      to="/"
+      > 
+          <ChevronRight/>
+      </Button>
+    </div>
+    )
+  }
+}
 
 export default Timetable;
